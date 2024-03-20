9de69548627b2787cd60b6fa45da4cfd94a0ad83
root <marsilas81@gmail.com>
2024-03-20 13:28:12 -0700

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,43 +1,20 @@
+#!/usr/bin/python3
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
 
-    roman_dict = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000
-    }
-
-    result = 0
-    prev_value = 0
+    roman_dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    result = prev_value = 0
 
     for symbol in reversed(roman_string):
         value = roman_dict[symbol]
-        if value < prev_value:
-            result -= value
-        else:
-            result += value
+        result += value if value >= prev_value else -value
         prev_value = value
 
     return result
 
 # Test cases
 if __name__ == "__main__":
-    roman_number = "X"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-    roman_number = "VII"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-    roman_number = "IX"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-    roman_number = "LXXXVII"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
-
-    roman_number = "DCCVII"
-    print("{} = {}".format(roman_number, roman_to_int(roman_number)))
+    test_cases = ["X", "VII", "IX", "LXXXVII", "DCCVII"]
+    for roman_number in test_cases:
+        print("{} = {}".format(roman_number, roman_to_int(roman_number)))
